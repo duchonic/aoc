22aad377a5ffcf2ce07783edc084236cde1f2d3b
Nicolas <duchonic@bluewin.ch>
2019-11-21 20:24:23 +0100

    up

--- 2016/day00/main.cpp
+++ 2016/day00/main.cpp
@@ -5,8 +5,12 @@
 #include <set>
 #include <cassert>
 #include <cmath>
+#include <map>
+#include <deque>
+#include <algorithm>
 
 #include "log.h"
+#include "json.h"
 
 /**
 * @brief process data in file with this struct
@@ -14,19 +18,61 @@
 */
 struct inputType{
 	std::string command;
+	unsigned int line = 0;
+	std::string nr = "";
+	std::string checksum = "";
+	std::string decoy = "";
+	std::string raw = "";
+	bool startChecksum = false;
+	
+	std::map<char, int> map;
+
 	friend std::istream& operator>>(std::istream& is, inputType& info){
 		is >> info.command;
+		info.line++;
+		info.nr = "";
+		info.checksum = "";
+		info.decoy = "";
+		info.raw = "";
+		info.map.clear();
+
+		for (char c : info.command) {
+			if (isdigit(c)) {
+				info.nr += c;
+			}
+			else if (c == '[') {
+				info.startChecksum = true;
+			}
+			else if (c == ']') {
+				info.startChecksum = false;
+			}
+			else if (info.startChecksum) {
+				info.checksum += c;
+			}
+			else {
+				info.raw += c;
+				if (c != '-') {
+					info.decoy += c;
+					
+					if( info.map.count(c) == 0){
+						info.map[c] = 1;
+					}
+					else{
+						info.map.at(c)++;
+					}
+				}
+			}
+		}
 		return is;
 	}
 };
 
 /**
-* @brief program that process some data from the file
+ * @brief program that process some data from the file
 *        mostly called twice with test.txt and data.txt
 *
 */
 void runProgram(std::string fileName) {
-
 	std::vector<inputType> v;
 
 	auto file = std::ifstream(fileName);
@@ -35,11 +81,6 @@ void runProgram(std::string fileName) {
 	for (inputType data; file >> data;) {
 		v.push_back(data);
 	}
-
-	DEBUG_LOG("data from " << fileName);
-	for (auto data : v) {
-		INFO_LOG(data.command);
-	}
 }
 
 /**
@@ -47,11 +88,8 @@ void runProgram(std::string fileName) {
 * 
 */
 int main(){
-
-	//std::cout << "aoc 2016 dayxy" << std::endl;
-	INFO_LOG("aoc 2016 day xy");
+	INFO_LOG("2016/6");
 	runProgram("../test.txt");
 	runProgram("../data.txt");
-
 	return 0;
 }
--- 2016/day06/main.cpp
+++ 2016/day06/main.cpp
@@ -5,11 +5,12 @@
 #include <set>
 #include <cassert>
 #include <cmath>
-#include <unordered_map>
-#include <algorithm>
 #include <map>
-#include <array>
+#include <deque>
+#include <algorithm>
+
 #include "log.h"
+#include "json.h"
 
 /**
 * @brief process data in file with this struct
@@ -17,76 +18,68 @@
 */
 struct inputType{
 	std::string command;
+	unsigned int line = 0;
+	std::string nr = "";
+	std::string checksum = "";
+	std::string decoy = "";
+	std::string raw = "";
+	bool startChecksum = false;
+	
+	std::map<char, int> map;
+
 	friend std::istream& operator>>(std::istream& is, inputType& info){
 		is >> info.command;
+		info.line++;
+		info.nr = "";
+		info.checksum = "";
+		info.decoy = "";
+		info.raw = "";
+		info.map.clear();
+
+		for (char c : info.command) {
+			if (isdigit(c)) {
+				info.nr += c;
+			}
+			else if (c == '[') {
+				info.startChecksum = true;
+			}
+			else if (c == ']') {
+				info.startChecksum = false;
+			}
+			else if (info.startChecksum) {
+				info.checksum += c;
+			}
+			else {
+				info.raw += c;
+				if (c != '-') {
+					info.decoy += c;
+					
+					if( info.map.count(c) == 0){
+						info.map[c] = 1;
+					}
+					else{
+						info.map.at(c)++;
+					}
+				}
+			}
+		}
 		return is;
 	}
 };
 
 /**
-* @brief program that process some data from the file
+ * @brief program that process some data from the file
 *        mostly called twice with test.txt and data.txt
 *
 */
 void runProgram(std::string fileName) {
-
 	std::vector<inputType> v;
-	std::array< std::map<char, uint16_t>, 8> cols;
 
 	auto file = std::ifstream(fileName);
 	assert(file.is_open());
 
 	for (inputType data; file >> data;) {
 		v.push_back(data);
-	}
-
-	DEBUG_LOG("data from " << fileName);
-	
-	std::unordered_map<char, uint16_t> count;
-	
-	for (auto data : v) {
-		INFO_LOG(data.command);
-		size_t index = 0;
-		for (char ch : data.command) {
-			cols[index++][ch]++;
-		}
-	}
-	std::string solution{ "" };
-	for (size_t index : {0, 1, 2, 3, 4, 5, 6, 7}) {
-		char maxChar;
-		uint16_t min = 50000;
-		for (auto ch : cols[index]) {
-			if (ch.second < min) {
-				min = ch.second;
-				maxChar = ch.first;
-			}
-		}
-		solution += maxChar;
-		INFO_LOG("col:" << index << " max char: " << maxChar);
-	}
-
-	DEBUG_LOG(solution);
-
-	for (auto data : v) {
-		count.clear();
-
-		for (char ch : data.command) {
-			count[ch]++;
-		}
-
-		using myPair = std::pair<char, unsigned int>;
-		auto comp = [](const myPair& a, const myPair& b) {
-			return (a.second > b.second || a.second == b.second && a.first < b.first);
-		};
-
-		std::set<myPair, decltype(comp) > sorted(comp);
-		for (auto const& kv : count)
-			sorted.insert(kv);
-
-		//DEBUG_LOG("Sorted list according to frequency then alphabetically");
-		for (auto const& kv : sorted){
-			//DEBUG_LOG( kv.first << " = " << kv.second );
-		}
 	}
 }
 
@@ -95,10 +88,8 @@ void runProgram(std::string fileName) {
 * 
 */
 int main(){
-	INFO_LOG("aoc 2016 day 06");
-
+	INFO_LOG("2016/6");
 	runProgram("../test.txt");
-	//runProgram("../data.txt");
-
+	runProgram("../data.txt");
 	return 0;
 }

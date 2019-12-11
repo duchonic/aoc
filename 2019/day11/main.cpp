31ee48a184b57ab3ecf2a5d25763add594e64d0f
Nicolas Duchoud <n.duchoud@micropool.ch>
2019-12-11 15:28:14 +0100

    201911

---
+++ 2019/day11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <set>
+#include <cassert>
+#include <cmath>
+#include "log.h"
+
+int main(){
+	INFO_LOG("201911");
+	
+
+	return 0;
+}

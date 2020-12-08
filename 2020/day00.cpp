9b07c26be9371bc72d8aa6f9717f776b94c06689
Nicolas Duchoud <n.duchoud@micropool.local>
2020-12-08 16:03:07 +0100

    day07 via python

--- 2020/day00.cpp
+++ 2020/day00.cpp
@@ -7,59 +7,124 @@
 
 struct Person{
 	char name;
-	std::set<Person> child;
+	std::vector<Person> child;
 };
 
 inline bool operator<(const Person& p1, const Person& p2){
   return p1.name < p2.name;
 }
 
-void show(Person p){
+void show(Person p, int childs){
 	std::cout << p.name << std::endl;
 	if(p.child.size()){
 		for(Person child: p.child){
-			std::cout << " - ";
-			show(child);
+			for(int spaces=0;spaces<childs;spaces++){
+				std::cout << ' ';
+			}
+			show(child, childs+1);
 		}
 	}
 }
 
+void checkParents(Person &parent, Person child){
+	std::cout << "check : " << parent.name << std::endl;
+	if(parent.child.size()){
+		// has some childrens ... 
+		for(Person &childrenName : parent.child){
+			// search the childrens ...
+			if( childrenName.name == child.name ){
+				std::cout << "parent found : " << childrenName.name << std::endl;	
+				std::cout << "check children of : " << childrenName.name << std::endl; 
+				//check childs of found parent
+				bool notAChild = true;
+				for(auto check : childrenName.child){
+					std::cout << "check " << check.name << " == " << childrenName.name << std::endl;
+					if(check.  == childrenName.name){
+						std::cout << "already a child" << std::endl;
+						notAChild = false;
+					}
+				}
+				if(notAChild){
+					Person newChild;
+					newChild.name = child.child.at(0).name;
+					childrenName.child.push_back(newChild);
+
+					break;
+				}
+			}
+			checkParents(childrenName, child);
+		}
+
+	}
+	
+}
+
+
 int main(){
 	std::ifstream inputFile("day00.txt");        // Input file stream object
-
 	if (inputFile.good()) {
 
-		std::set<Person> family;
+		Person genesis;
+
+		genesis.name = 'x';
+
 		char name, childname;
 		while(inputFile >> name >> childname){
-			//std::cout << name << ' ' << childname << std::endl;
 			Person actual;
 			Person child;
 			actual.name = name;
 			child.name = childname;
-			actual.child.insert(child);
+			actual.child.push_back(child);
 
-			for(Person person : family){
-				for(Person child: person.child){
-					if (child.name == name){
-						std::cout << "found a child" << std::endl;
-						child.child.insert(actual);
-					}
-				}
-			}
-			family.insert(actual);
+			std::cout << "got entry:" << std::endl;
+			show(actual, 1);
+
+			genesis.child.push_back(actual);
+
+			checkParents(genesis, actual);
+
+			std::cout << "show genesis" << std::endl;
+			show(genesis, 1);
 		}
 
 
-		for(Person p : family){
-			show(p);
-		}
+		
 	}
 	else{
 		std::cout << "file not found" << std::endl;
 	}
 
 
+
+
+	std::cout << "some stuff to test:::" << std::endl;
+
+	Person genesis;
+	genesis.name = 'a';
+	
+	Person child_b;
+	child_b.name = 'b';
+	Person child_c;
+	child_c.name = 'c';
+
+
+	Person grandchild;
+	grandchild.name = 'c';
+	child_b.child.push_back(grandchild);
+	child_c.child.push_back(grandchild);
+	genesis.child.push_back(child_b);
+	genesis.child.push_back(child_c);
+
+	
+	show(genesis, 1);
+
+	std::cout << "genesis childs" << std::endl;
+	for(auto child : genesis.child){
+		std::cout << child.name << std::endl;
+	}
+
+
 	return 0;
+
 }
 
---
+++ 2020/day07.py
@@ -0,0 +1,56 @@
+# Read in groups from file
+lines = [x.strip() for x in open('day07.txt', 'r').readlines() if x != '']
+
+print(lines)
+
+def part1():
+    bags = {}
+    q = []
+    for l in lines:
+        bag, contains = l.split('contain')
+        bag = bag.replace('bags','').strip()
+        if 'shiny gold' in contains:
+            q.append(bag)
+        bags[bag] = contains
+
+    answer = set(q)
+    while len(q) != 0:
+        nxt = q.pop(0)
+        for b in bags:
+            if nxt in bags[b]:
+                q.append(b)
+                answer.add(b)
+    return len(answer)
+
+def part2():
+    bags = {}
+    q = []
+    for l in lines:
+        bag, contains = l.split('contain')
+        bag = bag.replace('bags','').strip()
+
+        if 'no other bags' in contains:
+            bags[bag] = {}
+            continue
+
+        contains = contains.split(',')
+        contains = [c.replace('bags','').replace('bag','').replace('.','').strip() for c in contains]
+        x = {}
+        for c in contains:
+            x[c[2:].strip()] = int(c.split()[0])
+        bags[bag] = x 
+
+    def recurs(bag, bags, level):
+        subitems = bags[bag]
+        if len(subitems.keys()) == 0:
+            return 1
+
+        total = 1
+        for s in subitems:
+            total += subitems[s] * recurs(s, bags, level + 1) 
+        return total
+
+    return recurs('shiny gold', bags, 0)  - 1
+
+print (part1())
+print (part2())

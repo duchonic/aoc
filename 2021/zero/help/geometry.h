2c3686997e514f8e1c28b4096c4cefd179c74e9e
duchonic <duchonic@bluewin.ch>
2021-11-23 22:30:27 +0100

    2021 cpp zero added

---
+++ 2021/zero/help/geometry.h
@@ -0,0 +1,29 @@
+/****************** Geometry *****************/ 
+template <typename T> inline T PointDistanceHorVer(T x1,T y1,T x2, T y2) 
+{return abs(x1-x2)+abs(y1-y2);} 
+template <typename T> inline T PointDistanceDiagonally(T x1,T y1,T x2, T y2) 
+{return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));} 
+template <typename T> inline T PointDistanceMinimum(T x1,T y1,T x2, T y2) 
+{ T tmp1=abs(x1-x2); T tmp2=abs(y1-y2); T tmp3=abs(tmp1-tmp2); 
+T tmp4=min(tmp1, tmp2); return tmp3+tmp4; } 
+template <typename T> inline T PointDistance3D(T x1,T y1,T z1,T x2,T y2,T z2)
+{return sqrt(square(x2-x1)+square(y2-y1)+square(z2-z1));} 
+ 
+template <typename T> inline T Cube(T a){return a*a*a;} 
+template <typename T> inline T RectengularPrism(T a,T b,T c)
+{return a*b*c;} 
+template <typename T> inline T Pyramid(T base, T height)
+{return (1/3)*base*height;} 
+template <typename T> inline T Ellipsoid(T r1,T r2,T r3) 
+{return (4/3)*PI*r1*r2*r3;} 
+template <typename T> inline T IrregualarPrism(T base, T height)
+{return base*height;} 
+template <typename T> inline T Sphere(T radius)
+{ return (4/3)*PI*radius*radius*radius;} 
+template <typename T> inline T CylinderB(T base, T height)
+{return base*height;} // base and height 
+template <typename T> inline T CylinderR(T radius, T height)
+{return PI*radius*radius*height;} // radius and height 
+template <typename T> inline T Cone (T radius,T base, T height)
+{return (1/3)*PI*radius*radius*height;} 
+/****************** Geometry end *****************/ 
---
+++ 2021/zero/help/prime.h
@@ -0,0 +1,15 @@
+/****************** Prime Generator **********************/ 
+const int N=1e7+10; int prime[20000010]; 
+bool isprime[N]; int nprime; 
+template <typename T> void Sieve(T a) 
+{nprime = 0;memset(isprime,true,sizeof(isprime));
+isprime[1]=false;for(int i=2;i<N;i++){
+if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
+isprime[i*j]=false;}}}
+
+template <typename T> inline T PrimeFactors(T n)
+{ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
+and i<nprime;i++){cnt=0;while(n%prime[i]==0)
+{cnt++;n/=prime[i];}sum*=(cnt+1);}
+if(n>1)sum*=2;return sum;} 
+/****************** Prime Generator End **********************/ 

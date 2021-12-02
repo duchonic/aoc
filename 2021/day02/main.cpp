ca45a098ef1860763be0c000163e5d1a2120d9d5
duchonic <duchonic@bluewin.ch>
2021-12-02 12:00:34 +0100

    day02

--- 2021/day02/main.cpp
+++ 2021/day02/main.cpp
@@ -13,19 +13,79 @@ _|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"
 #include "help/geometry.h"
 #include "help/string.h"
 #include "help/file.h"
+#include "help/log.h"
+
+typedef struct {
+	int32_t x;
+	int32_t y;
+	int32_t aim;
+}Position;
+
+static int32_t calcPath( std::vector<std::string> input) {
+	Position pos = {
+		.x = 0,
+		.y = 0,
+	};
+
+	for (auto line: input) {
+		std::stringstream s(line);
+		std::string direction;
+		int16_t distance;
+
+		s >> direction;
+		s >> distance;
+		
+		if (direction.compare("forward") == 0) {
+			pos.x += distance;
+		}
+		else if (direction.compare("up") == 0) {
+			pos.y -= distance;
+		}
+		else if (direction.compare("down") == 0) {
+			pos.y += distance;
+		}
+	}
+	log("endpos x:" << pos.x << " y:" << pos.y);
+	return (pos.x * pos.y);
+}
+static int32_t calcPath2( std::vector<std::string> input) {
+	Position pos = {
+		.x = 0,
+		.y = 0,
+		.aim = 0,
+	};
+
+	for (auto line: input) {
+		std::stringstream s(line);
+		std::string direction;
+		int16_t distance;
+
+		s >> direction;
+		s >> distance;
+		
+		if (direction.compare("forward") == 0) {
+			pos.x += distance;
+			pos.y += pos.aim * distance;
+		}
+		else if (direction.compare("up") == 0) {
+			pos.aim -= distance;
+		}
+		else if (direction.compare("down") == 0) {
+			pos.aim += distance;
+		}
+	}
+	log("endpos x:" << pos.x << " y:" << pos.y);
+	return (pos.x * pos.y);
+}
+
 
 int main() {
-	std::vector<std::string> data = readstuff();
 
-	int16_t part1 = 0;
 	int16_t part2 = 0;
-	int16_t linenumber = 0;
-	for (auto line: data) {
-		std::cout << line << std::endl;
-	}
 	
-	std::cout << "part1 : " << part1 << std::endl;
-	std::cout << "part2 : " << part2 << std::endl;
+	std::vector<std::string> data = readstuff();
+	std::cout << "part1 : " << calcPath(data) << std::endl;
+	std::cout << "part2 : " << calcPath2(data) << std::endl;
 
 	return 1;
 }

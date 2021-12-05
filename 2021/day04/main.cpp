cbb75490d7207e3dfd5a32fd9336ccc74b825cdd
duchonic <duchonic@bluewin.ch>
2021-12-05 16:38:03 +0100

    day4 two stars

--- 2021/day04/main.cpp
+++ 2021/day04/main.cpp
@@ -16,7 +16,8 @@ _|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"""""_|"
 #include "help/string.h"
 
 
-std::vector< std::array< std::array<int, 5> , 5>> cards;
+std::vector< std::array< std::array<int, 6> , 6>> cards;
+std::vector<std::string> sequence;
 
 static void printCards() {
 	for (auto card : cards){
@@ -30,9 +31,71 @@ static void printCards() {
 	}
 }
 
+static void checkCards() {
+	for (auto check : sequence) {
+		logger(" ");
+		logger("check for: " << check);
+		int cardNumber = -1;
+		std::vector<int> eraseCard;
+		for (auto card : cards){
+			cardNumber++;
+			int lineNumber = -1;
+			//for (auto line : card) {
+			for (int line=0; line<5; line++) {
+				lineNumber++;
+				int match = 0;
+				//int actualNumber = 0;
+				//for (auto nr : line) {
+				for (int actualNumber = 0; actualNumber < 5; actualNumber++) {
+					//actualNumber++;
+					if (stoi(check) == cards.at(cardNumber).at(lineNumber).at(actualNumber) ) {
+						cards.at(cardNumber).at(lineNumber).at(actualNumber) = -1;
+						cards.at(cardNumber).at(lineNumber).at(5)++;
+						cards.at(cardNumber).at(5).at(actualNumber)++;
+						if (cards.at(cardNumber).at(lineNumber).at(5) == 5 ||
+							cards.at(cardNumber).at(5).at(actualNumber) == 5 ) {
+							eraseCard.push_back(cardNumber);
+							int sum = 0;
+							//for (auto line : cards.at(cardNumber)) {
+							for (int y=0; y<5; y++) {
+								for (int index=0; index<5; index++){
+									if ( cards.at(cardNumber).at(y).at(index) != -1 ) {
+										sum += cards.at(cardNumber).at(y).at(index);
+									}
+								}
+							}
+							logger( "found it, actual check:" << check);
+							logger( "sum : " << sum);
+							logger( stoi(check) * sum );
+						}
+					}
+				}
+			}
+		}
+
+		auto iterator = std::remove_if(cards.begin(), cards.end(), [](std::array< std::array<int,6>,6> card){
+			for ( auto nr : card.at(5)) {
+				if (nr >= 5) {
+					return true;
+				}
+			}
+			
+			for ( auto line : card) {
+				if (line.at(5) >= 5) {
+					return true;
+				}
+			}
+			return false;
+		} );
+		cards.erase(iterator, cards.end());
+		if (cards.size() == 0) {
+			return;
+		}	
+	}	
+}
+
 int main() {
 	std::vector<std::string> data = readstuff();
-	std::vector<std::string> sequence;
 
 	split_str(data.front(), ',', sequence);
 	logger("sequence:");
@@ -43,21 +106,23 @@ int main() {
 
 	int actualCard = -1;
 	for (int line=2; line < data.size(); line+= 6) {
-		std::array<std::array<int, 5>,5> empty;
+		std::array<std::array<int, 6>,6> empty;
 		cards.push_back(empty);
 		actualCard++;	
 		for (int y=0; y<5; y++) {
 			std::stringstream ss(data.at(line+y));
-
+			cards.at(actualCard).at(y).at(5) = 0;
 			for (int count=0; count < 5; count++) {
 				int number;
 				ss >> number;
 				cards.at(actualCard).at(y).at(count) = number;
+				cards.at(actualCard).at(5).at(count) = 0;
 			}
 		}
 	}
 
-	printCards();
+	//printCards();
+	checkCards();
 
 	int16_t part1 = 0;
 	int16_t part2 = 0;

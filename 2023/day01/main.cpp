fa502427130ecaf9a2d50636a54d141f69deac4d
duchonic <duchonic@bluewin.ch>
2023-12-01 09:18:03 +0200

    2023 day01 part 1

--- 2023/day01/main.cpp
+++ 2023/day01/main.cpp
@@ -14,15 +14,39 @@
 #include "help/file.h"
 #include "help/log.h"
 
+#include <regex>
+#include <string>
 
 /**
  * @brief solves the problem
  */
 static int64_t solve(std::vector<std::string> input) {
-	int16_t returnValue = 0;
+	int64_t returnValue = 0;
 
 	for (auto line : input) {
-		std::cout << line << std::endl;
+
+		//std::cout << "solved with reges: " << std::regex_replace(line, std::regex(R"([a-z])"), "")  << std::endl;
+		auto solve_line = line;
+		std::vector<std::string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+		int index = 0;
+		for (auto &number : numbers) {
+			solve_line = std::regex_replace(solve_line, std::regex(number), std::to_string(index));
+			//std::cout << "number: " << number << " index: " << index << std::endl;
+			index++;
+		}
+		std::cout << "solved with reges: " << solve_line << std::endl;
+		auto value = std::regex_replace(solve_line, std::regex(R"([a-z])"), "");
+
+		auto first = value.begin();
+		auto last = value.end()-1;
+
+		std::cout << "first: " << *first << " last: " << *last << std::endl;
+		
+		returnValue += *last - '0';
+		returnValue += (*first - '0') * 10;
+		std::cout << "return value: " << returnValue << std::endl;
+
 	}
 	return returnValue;	
 }
@@ -30,11 +54,11 @@ static int64_t solve(std::vector<std::string> input) {
 
 int main() {
 	std::vector<std::string> data = readstuff();
-	std::cout << "solve part 1" << std::endl;
-	int16_t part1 = solve(data);
-	std::cout << "solve part 2" << std::endl;
-	int16_t part2 = solve(data);
+	std::cout << "2023 day01 solve part 1" << std::endl;
+	int64_t part1 = solve(data);
+	//std::cout << "2023 day01 solve part 2" << std::endl;
+	int64_t part2 = solve(data);
 	std::cout << "part1 : " << part1 << std::endl;
-	std::cout << "part2 : " << part2 << std::endl;
+	//std::cout << "part2 : " << part2 << std::endl;
 	return 0;
 }

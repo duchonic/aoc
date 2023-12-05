91c90cbf3512ad792553c0e52a96e13f1f12afb2
duchonic <duchonic@bluewin.ch>
2023-12-05 20:26:50 +0200

    day5

--- 2023/day05/main.cpp
+++ 2023/day05/main.cpp
@@ -25,11 +25,80 @@
 #include "help/plot.h"
 
 
+int64_t solve( std::array<int, 3> input, int64_t seed) {
+	int64_t returnValue = seed;
+
+	int64_t destination = input.at(0);
+	int64_t source = input.at(1);
+	int64_t length = input.at(2);
+
+	if (seed >= source && seed <= source + length) {
+
+		if (destination > source)
+			returnValue = seed + (destination - source);
+		else 
+			returnValue = seed - (source - destination);
+
+	}
+	return returnValue;
+}
+
+
 /**
  * @brief solves the problem
  */
 static int64_t solve(std::vector<std::string> input, bool DoPart2) {
 	int64_t returnValue = 0;
+
+
+	std::vector<int> seeds;
+	std::vector<std::array<int, 3>> commands;
+
+	/**
+	 * commands will always be 
+	 * destination, source, length
+	 *
+	 * example (50, 98, 2) will copy 2 bytes from 98 to 50
+	 *  
+	*/
+	for (auto line : input) {
+
+		std::vector<std::string> split;
+		split_str(line, ':', split);
+
+
+		if (split.size()) {
+			if (split.at(0) == "seeds") {
+				std::vector<std::string> split_seeds;
+				split_str(split.at(1), ' ', split_seeds);
+				for (auto seed : split_seeds) {
+					seeds.push_back(std::atoi(seed.c_str()));
+				}
+			}
+			if (isdigit(split.at(0)[0])) {
+				std::cout << "line : " << line << std::endl;
+				std::vector<std::string> split_command;
+				split_str(split.at(0), ' ', split_command);
+
+				std::array<int, 3> command_array = {std::atoi(split_command.at(0).c_str()), std::atoi(split_command.at(1).c_str()), std::atoi(split_command.at(2).c_str())};
+				commands.push_back(command_array);
+			}
+		}
+	}
+
+
+	for (auto seed : seeds) {
+
+		int calc_seed = seed;
+
+		std::cout << "seed: ";
+		for (auto command : commands) {
+
+			calc_seed = solve(command, calc_seed);
+			std::cout << " -> " << calc_seed; 
+		}
+		std::cout << std::endl;
+	}
 
 
 	return returnValue;	

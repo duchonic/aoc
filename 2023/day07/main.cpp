3463bdcc268fe656b6ac976244928a3e466a887b
duchonic <duchonic@bluewin.ch>
2023-12-06 19:57:44 +0100

    day07 prepare

---
+++ 2023/day07/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-07
+ * 
+ * @copyright codestation (c) 2023
+ *                   _                 _             _       __
+ *   ___    ___    _| |   ___    ___  | |_    _ _   | |_    (__)   ___    _ _   
+ *  / _ |  / _ \  /_` |  / -_)  (_-<  |  _|  /_` |  |  _|   |  |  / _ \  | ' \ 
+ *  \___|  \___/  \_,_|  \___|  /__/   \__|  \_,_|   \__|   |__|  \___/  |_||_|
+ * /""""\_/""""\_/""""\_/".."\_/""""\_/""""\_/""""\_/""""\_/""""\_/""""\_/""""\
+ * -0--0-=-0--0-=-0--0-=-0xx0-=-0--0-=-0--0-=-0--0-=-0--0-=-0--0-=-0--0-=-0--0-
+*/
+
+#include "help/help.h"
+#include "help/math.h"
+#include "help/bitwise.h"
+#include "help/prime.h"
+#include "help/geometry.h"
+#include "help/string.h"
+#include "help/file.h"
+#include "help/log.h"
+#include "help/plot.h"
+
+
+/**
+ * @brief solves the problem
+ */
+static int64_t solve(std::vector<std::string> input, bool DoPart2) {
+	int64_t returnValue = 1;
+
+
+
+	return returnValue;	
+}
+
+
+int main() {
+	std::vector<std::string> data = readstuff();
+	std::cout << "2023 day05 solve part 1" << std::endl;
+	int64_t part1 = solve(data, false);
+	std::cout << "part1 : " << part1 << std::endl;
+
+	std::cout << "2023 day05 solve part 2" << std::endl;
+	int64_t part2 = solve(data, true);
+	std::cout << "part2 : " << part2 << std::endl;
+	return 0;
+}

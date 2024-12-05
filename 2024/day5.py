2f52c0b490c270cb68d49ab411e9d808883c3d7a
Nicolas Duchoud <n.duchoud@priming.ch>
2024-12-05 19:33:57 +0100

    day5 part2

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,6 +1,8 @@
 
-def read_file(file):
-    part1 = 0
+import random
+
+def read_file(file, part):
+    result = 0
     rules = []
     sequence = []
     with open(file) as f:
@@ -8,7 +10,6 @@ def read_file(file):
         lines = f.readlines()
         for line in lines:
             values = line.replace("|", " ").replace(",", " ").split() 
-
 
             if len(values) == 2:
                 single = []
@@ -34,41 +35,63 @@ def read_file(file):
                     point = [rule[0], [rule[1]]]
                     tree.append(point)
 
-                
+        if part == 2: 
+            print(tree)
 
-        for seq in sequence:
-            neighbour_found = False
-            for number in zip(seq[::], seq[1::]):
+        if part == 1:
+            for seq in sequence:
                 neighbour_found = False
-                for point in tree:
-                    if number[0] == point[0]:
-                        for value in point[1]:
-                            if number[1] == value:
-                                neighbour_found = True
+                for number in zip(seq[::], seq[1::]):
+                    neighbour_found = False
+                    for point in tree:
+                        if number[0] == point[0]:
+                            for value in point[1]:
+                                if number[1] == value:
+                                    neighbour_found = True
+                    if not neighbour_found:
+                        break;
                 if not neighbour_found:
-                    break;
-            if not neighbour_found:
-                pass
-            else:
-                part1 += seq[int(len(seq)/2)]
+                    pass
+                else:
+                    if part == 1:
+                        result += seq[int(len(seq)/2)]
+        else:
+            for seq in sequence:
 
+                test_seq = seq.copy()
 
-    return part1 
-
-    
-
+                repaired = False
+                while True:
+                    neighbour_found = False
+                    for number in zip(test_seq[::], test_seq[1::]):
+                        neighbour_found = False
+                        for point in tree:
+                            if number[0] == point[0]:
+                                for value in point[1]:
+                                    if number[1] == value:
+                                        neighbour_found = True
+                        if not neighbour_found:
+                            break;
+                    
+                    if not neighbour_found:
+                        repaired = True
+                        #random shuffle the test_seq
+                        #print(test_seq)
+                        random.shuffle(test_seq)
+                    else:
+                        if repaired:
+                            print(test_seq)
+                            print(test_seq[int(len(test_seq)/2)])
+                            result += test_seq[int(len(test_seq)/2)]
+                        break
+    return result 
 
 def main():
+    assert(read_file("day5_test.txt", 1) == 143)
+    print("part1: ", read_file("day5.txt", 1))
 
-    test = read_file("day5_test.txt")
-
-    assert(test == 143)
-
-    part1 = read_file("day5.txt")
-    print("part1: ", part1)
-
+    assert(read_file("day5_test.txt", 2) == 123)
+    print("part2: ", read_file("day5.txt", 2))
 
 if __name__ == '__main__':
     main()
-
-
